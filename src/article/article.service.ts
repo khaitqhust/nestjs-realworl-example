a1cf40e3068027d653ab471b4517f34cc2e069fb
Khai Tran <khaitq@cystack.net>
2022-01-08 18:09:43 +0700

    chore: update article service

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from "@nestjs/typeorm";
 import { ArticleEntity } from "./article.entity";
-import { Repository } from "typeorm";
+import { getRepository, Repository } from "typeorm";
 import { CommentEntity } from "./comment.entity";
 import { UserEntity } from "../user/user.entity";
 import { FollowsEntity } from "../profile/follows.entity";
+import { ArticlesRO } from "./article.interface";
 
 @Injectable()
 export class ArticleService {
@@ -19,5 +20,34 @@ export class ArticleService {
     private readonly followsRepository: Repository<FollowsEntity>
   ) {}
 
-  
+  async findAll(query): Promise<ArticlesRO> {
+    const qb = await getRepository(ArticleEntity).createQueryBuilder("article").leftJoinAndSelect("article.author", "author");
+    qb.where("1 = 1")
+
+    if ("tag" in query) {
+      qb.andWhere("article.tagList LIKE :tag", {tag: `%${query.tag}%`})
+    }
+    if ("author" in query) {
+      const author = await this.userRepository.findOne({username: query.author})
+      qb.andWhere("article.authorId = :id", {id: author.id})
+    }
+    if ("favorited" in query) {
+      const author = await this.userRepository.findOne({username: query.favorited})
+      const ids = author.favorites.map(el => el.id)
+      qb.andWhere("article.authorId IN (:ids)", {ids})
+    }
+    qb.orderBy("article.created", "DESC")
+
+    const articlesCount = await qb.getCount()
+
+    if ("limit" in query) {
+      qb.limit(query.limit)
+    }
+    if ("offset" in query) {
+      qb.offset(query.offset)
+    }
+
+    const articles = await qb.getMany()
+    return {articles, articlesCount}
+  }
 }
